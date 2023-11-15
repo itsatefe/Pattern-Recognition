40f70c1adff837da6eaecfa44493f25f2ec96d3f
Atefe Rajabi <atefe.rajabi.78@gmail.com>
2023-11-16 03:09:21 +0330

    QDA model

--- HW2/Binary Classification using Beyesian Classifier/Model.py
+++ HW2/Binary Classification using Beyesian Classifier/Model.py
@@ -38,13 +38,6 @@ class GaussianLinearClassifier:
         sigma_inv = np.linalg.inv(covariance)
         likelihood_matrix = [coefficient * np.exp(-0.5 * np.dot(np.dot((sample - mean), sigma_inv), (sample - mean).T)) for sample in x]
         return likelihood_matrix
-    
-    def accuracy(self, y_true, y_pred):
-        correct_predictions = np.sum(y_true == y_pred)
-        total_samples = len(y_true)
-        accuracy = correct_predictions / total_samples
-        return accuracy
-
 
     def mahalanobis_distance(self, x, mean, covariance):
         sigma_inv = np.linalg.inv(covariance)
@@ -57,5 +50,52 @@ class GaussianLinearClassifier:
         predictions = np.argmin(a, axis=1)
         return predictions
 
+class QuadraticDiscriminantAnalysis:
+    def __init__(self):
+        self.pi_mle = None
+        self.mu_mle = None
+        self.sigma_mle = None
+        self.num_class = None
 
+    def fit(self, X, y):
+        self.num_class = np.unique(y)
+        self.pi_mle = self.pi_mle_j(y)
+        self.mu_mle = [np.mean(X[y == i], axis=0) for i in self.num_class]
+        self.sigma_mle = self.mle_covariance_matrix(X, y)
+
+    def predict_likelihood(self, X):
+        likelihood = [self.likelihood(X, self.mu_mle[i], self.sigma_mle[i]) for i in range(len(self.num_class))]
+        posterior = np.column_stack([self.pi_mle[i] * np.array(likelihood[i]) for i in range(len(self.num_class))])
+        predictions = np.argmax(posterior, axis=1)
+        return predictions
+
+    def pi_mle_j(self, y_j):
+        m = len(y_j)
+        pi_j = [np.sum(y_j == i) / m for i in self.num_class]
+        return pi_j
+
+    def mle_covariance_matrix(self, data, labels):
+        unique_labels = np.unique(labels)
+        # normalized 1 / n-1   
+        covariance_matrices = [ (1 / (len(data[labels == i])-1)) * np.dot((data[labels == i] - np.mean(data[labels == i], axis=0)).T, data[labels == i] - np.mean(data[labels == i], axis=0)) for i in unique_labels]
+        return covariance_matrices
+
+    def likelihood(self, x, mean, covariance):
+        D = len(mean)
+        cov_det = np.linalg.det(covariance)
+        coefficient = 1 / ((2 * np.pi) ** (D / 2) * np.sqrt(cov_det))
+        sigma_inv = np.linalg.inv(covariance)
+        likelihood_matrix = [coefficient * np.exp(-0.5 * np.dot(np.dot((sample - mean), sigma_inv), (sample - mean).T)) for sample in x]
+        return likelihood_matrix
+    
+    def mahalanobis_distance(self, x, mean, covariance):
+        sigma_inv = np.linalg.inv(covariance)
+        distances = [np.dot(np.dot((sample - mean), sigma_inv), (sample - mean).T) for sample in x]
+        return distances
+    
+    def predict_distance(self, X):
+        mahalanobis_distances = [self.mahalanobis_distance(X, self.mu_mle[i], self.sigma_mle[i]) for i in range(len(self.num_class))]
+        a = np.column_stack([mahalanobis_distances[i] for i in range(len(self.num_class))])
+        predictions = np.argmin(a, axis=1)
+        return predictions
 
