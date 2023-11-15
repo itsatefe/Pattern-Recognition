eb3b030cc8f39d77ac7000d76734738a0c4e0861
Atefe Rajabi <atefe.rajabi.78@gmail.com>
2023-11-16 00:57:36 +0330

    GLDA End

---
+++ HW2/Binary Classification using Beyesian Classifier/Metrics.py
@@ -0,0 +1,62 @@
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+class Metrics:
+    def __init__(self, y_true):
+        self.y_true = np.array(y_true)
+        self.num_class = y_true.unique()
+
+    def confusion_matrix(self,y_pred):
+        unique_labels = np.unique(np.concatenate((self.y_true, y_pred)))
+        matrix = np.zeros((len(unique_labels), len(unique_labels)), dtype=int)
+        for true, pred in zip(self.y_true, y_pred):
+            matrix[true, pred] += 1
+        return matrix
+
+    def accuracy(self,y_pred):
+        cm = self.confusion_matrix(y_pred)
+        return np.trace(cm) / np.sum(cm)
+
+    def precision(self,y_pred):
+        cm = self.confusion_matrix(y_pred)
+        prec = []
+        for label in range(len(self.num_class)):
+            true_positive = cm[label, label]
+            false_positive = np.sum(cm[:, label]) - true_positive
+            prec.append(true_positive / (true_positive + false_positive))
+        return prec 
+
+    def recall(self,y_pred):
+        cm = self.confusion_matrix(y_pred)
+        rec = []
+        for label in range(len(self.num_class)):
+            true_positive = cm[label, label]
+            false_negative = np.sum(cm[label, :]) - true_positive
+            rec.append(true_positive / (true_positive + false_negative))
+        return rec
+
+    def f1_score(self,y_pred):
+        precision_value = self.precision(y_pred)
+        recall_value = self.recall(y_pred)
+        f1 = []
+        for label in range(len(self.num_class)):
+            f1.append(2 * (precision_value[label] * recall_value[label]) / (precision_value[label] + recall_value[label]))
+        return f1
+    
+    def misclassified_samples(self,y_pred):
+        cm = self.confusion_matrix(y_pred)
+        return np.sum(cm) - np.trace(cm)
+    
+    def display_confusion_matrix(self,y_pred,type_data):
+        colors=["#2ecc71", "#e74c3c"]
+        plt.figure(figsize=(2, 2))
+        cm = self.confusion_matrix(y_pred)
+        sns.heatmap(cm, annot=True, fmt="d", cmap=colors, cbar=False, xticklabels=["0", "1"], yticklabels=["0", "1"])
+        plt.xlabel("Predicted")
+        plt.ylabel("Actual")
+        plt.title(type_data)
+        plt.show()
+
+
+
---
+++ HW2/Binary Classification using Beyesian Classifier/Model.py
@@ -0,0 +1,61 @@
+import numpy as np
+
+class GaussianLinearClassifier:
+    def __init__(self):
+        self.pi_mle = None
+        self.mu_mle = None
+        self.sigma_mle = None
+        self.num_class = None
+
+    def fit(self, X, y):
+        self.num_class = y.unique()
+        self.pi_mle = self.pi_mle_j(y)
+        self.mu_mle = [np.mean(X[y == i], axis=0) for i in range(len(self.num_class))]
+        self.sigma_mle = self.mle_covariance_matrix(X)
+
+    def predict_likelihood(self, X):
+        likelihood = [self.likelihood(X, self.mu_mle[i], self.sigma_mle) for i in range(len(self.num_class))]
+        posterior = np.column_stack([self.pi_mle[i] * np.array(likelihood[i]) for i in range(len(self.num_class))])
+        predictions = np.argmax(posterior, axis=1)
+        return predictions
+
+    def pi_mle_j(self, y_j):
+        m = len(y_j)
+        pi_j = [sum(y_j == i) / m for i in range(len(self.num_class))]
+        return pi_j
+
+    def mle_covariance_matrix(self, data):
+        mean_vector = np.mean(data, axis=0)
+        diff_matrix = data - mean_vector
+        # normalized 1 / n-1
+        mle_cov_matrix = (1 / (len(data)-1)) * np.dot(diff_matrix.T, diff_matrix)
+        return mle_cov_matrix
+
+    def likelihood(self, x, mean, covariance):
+        D = len(mean)
+        cov_det = np.linalg.det(covariance)
+        coefficient = 1 / ((2 * np.pi) ** (D / 2) * np.sqrt(cov_det))
+        sigma_inv = np.linalg.inv(covariance)
+        likelihood_matrix = [coefficient * np.exp(-0.5 * np.dot(np.dot((sample - mean), sigma_inv), (sample - mean).T)) for sample in x]
+        return likelihood_matrix
+    
+    def accuracy(self, y_true, y_pred):
+        correct_predictions = np.sum(y_true == y_pred)
+        total_samples = len(y_true)
+        accuracy = correct_predictions / total_samples
+        return accuracy
+
+
+    def mahalanobis_distance(self, x, mean, covariance):
+        sigma_inv = np.linalg.inv(covariance)
+        distances = [np.dot(np.dot((sample - mean), sigma_inv), (sample - mean).T) for sample in x]
+        return distances
+
+    def predict_distance(self, X):
+        mahalanobis_distances = [self.mahalanobis_distance(X, self.mu_mle[i], self.sigma_mle) for i in range(len(self.num_class))]
+        a = np.column_stack([mahalanobis_distances[i] for i in range(len(self.num_class))])
+        predictions = np.argmin(a, axis=1)
+        return predictions
+
+
+
